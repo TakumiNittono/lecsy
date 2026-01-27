667139e2438fc5105bf269edaa724af5660eaeab
TakumiNittono <nittonotakumi@gmail.com>
2026-01-26 19:20:22 -0500

    Initial Commit

---
+++ lecsy/lecsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecsyApp.swift
+//  lecsy
+//
+//  Created by Takuminittono on 2026/01/26.
+//
+
+import SwiftUI
+
+@main
+struct lecsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lecsyTests/lecsyTests.swift
@@ -0,0 +1,17 @@
+//
+//  lecsyTests.swift
+//  lecsyTests
+//
+//  Created by Takuminittono on 2026/01/26.
+//
+
+import Testing
+@testable import lecsy
+
+struct lecsyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
